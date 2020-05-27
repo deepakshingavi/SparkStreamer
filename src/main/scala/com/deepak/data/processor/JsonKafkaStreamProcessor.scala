5d0645da910907eb84c9d14b289c5f299d7030cd
Deepak S <deepak@ridecell.com>
2020-05-27 16:23:16 +0530

    ading descirption to the code

--- src/main/scala/com/deepak/data/processor/JsonKafkaStreamProcessor.scala
+++ src/main/scala/com/deepak/data/processor/JsonKafkaStreamProcessor.scala
@@ -26,36 +26,57 @@ object JsonKafkaStreamProcessor {
    */
   def main(args: Array[String]): Unit = {
     val spark = Constant.getSparkSess
+
+    //Create and load you Spark Dataframe from Kafka
     val kafkaDf: DataFrame = spark.readStream.format("kafka")
       .option("subscribe", "topic-name")
       .option("kafka.bootstrap.servers", "kafkahost:port")
       .load()
 
-    //minimal schema field which I am expecting from my json
+    //minimal dummy schema field which I am expecting from my json
     //replace customer_id with your mandatory field
     val schema = new StructType()
       .add("customer_id", StringType)
+
+    //implicit format to parse JSON for json4s APIs
     implicit val formats: DefaultFormats.type = DefaultFormats
+
+
+    //Spark's Out stream of the
     val outputDf = kafkaDf
+      //Select kafka event as String
       .select(col("value").cast("string").as("value"),
+        //Parse JSON string to `data` column
         from_json(col("value").cast("string"), schema).as("data"))
+      //Select only value from data field in a column "value"
       .select("value", "data.*")
       .writeStream
+      //Fail safe
       .option("checkpointLocation", "/checkpoint/dir/")
+
+      //Stream job will be executed every 1 second
       .trigger(Trigger.ProcessingTime("1 second"))
+
+      //Iterate every 1 second batch as a individual Dataset
       .foreachBatch((dsw: Dataset[_], batchId: Long) => {
         val recordsCount = dsw.count()
         rootLogger.info(s"""DataFrame record count="${recordsCount}" batchId=${batchId} """)
 
+        //Grouping the dataset by Customer ID int a List
         dsw.groupBy("customer_id")
           .agg(collect_list("value").cast(ArrayType(StringType)).as("value"))
           .foreach((row: Row) => {
-            val tenantId = row.getAs[String]("tenant_id")
+            //You can get the customer directly
+            val customerId = row.getAs[String]("customer_id")
+
+            //JSOn event are still List<String> so iterate 
             val data = row.getAs[mutable.WrappedArray[String]]("value")
             data
               .array
               .map(event => {
+                //Parse individual JSON using json4s APIs
                 val parsedJson = parse(event)
+                //query JSON to extract nested values
                 val timeStampStr: String = (parsedJson \ "timestamp").extract[String]
                 val user_id: String = Try((parsedJson \ "user_id").extract[String]).getOrElse("no_user_")
                 rootLogger.info(s"timeStampStr=${timeStampStr} user_id=${user_id}")
